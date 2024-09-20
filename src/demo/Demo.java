756f6fa62aed9fe559d4e76546ae42960549edba
Purval09 <papurval@sheridancollege.ca>
2024-09-20 12:32:13 -0400

    Update Demo.java

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -14,6 +14,8 @@ public class Demo {
      * @param args the command line arguments
      */
     //gfghf
+
+    
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello java" + " you are king of annoying");
